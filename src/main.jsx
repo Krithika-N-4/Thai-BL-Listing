db0144da69d9aa930e5e5b30627aafe2c3dfe6a1
Krithika-N-4 <krithika.lakshmipriya@gmail.com>
2025-06-29 17:40:20 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Thai-BL-Listing">
       <App />
     </BrowserRouter>
   </StrictMode>,
